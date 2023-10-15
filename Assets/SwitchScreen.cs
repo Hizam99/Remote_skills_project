4f1cb7f261bbed86809314c73b4ba4ee67e2319e
Patrick <s4650048@uq.edu.au>
2023-10-15 17:06:46 +1000

    trying to merge to main

--- Assets/SwitchScreen.cs
+++ Assets/SwitchScreen.cs
@@ -6,6 +6,8 @@ using System.Net;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.Device;
 
 public enum screenSelector
 {
@@ -13,7 +15,7 @@ public enum screenSelector
     student,
     teacher
 }
-//Please work
+
 public class SwitchScreen : MonoBehaviour
 {
     public GameObject loginScreen;
@@ -24,9 +26,12 @@ public class SwitchScreen : MonoBehaviour
     public GameObject buttonCover;
     public GameObject IPTextBox;
     public TextMeshProUGUI IPDisplay;
+    public GameObject help_page;
     private GameObject current;
-    private bool alert;
-    private float time = 0;
+    
+    private bool recip = false;
+    private int helpPageNum = 0;
+
 
 
     public void Start()
@@ -39,14 +44,12 @@ public class SwitchScreen : MonoBehaviour
         current.SetActive(true);
     }
 
-
     public void teacherMode()
     {
         callingSession.SetActive(true);
         overlappingElement.SetActive(true);
         changeScreen(screenSelector.teacher);
     }
-
 
     //This function is to show the button cover and the IP text box
     //Activated by pressing student or teacher button
@@ -96,32 +99,51 @@ public class SwitchScreen : MonoBehaviour
         changeScreen(screenSelector.login);
     }
 
-    public void Alert()
+    public void recipe()
     {
-        alert = true;
-    }
-
-    public void test()
-    {
-        print("working");
-    }
-
-    private void Update()
-    {
-        if (alert)
+        if (recip == false)
         {
-            overlappingElement.transform.GetChild(2).gameObject.SetActive(true);
+            recip = true;
+        } else
+        {
+            recip = false;
         }
 
-        if (time < 1 && alert)
+        overlappingElement.transform.GetChild(5).gameObject.SetActive(recip);
+    }
+
+    public void instructionPage(string action)
+    {
+        switch (action)
         {
-            time = time + Time.deltaTime;
-        } else if (time > 1 && alert)
-        {
-            alert = false;
-            time = 0;
-            overlappingElement.transform.GetChild(2).gameObject.SetActive(false);
-            time = 0;
+            case "open":
+                help_page.transform.GetChild(1).gameObject.SetActive(true);
+                break;
+            case "close":
+                help_page.transform.GetChild(1).gameObject.SetActive(false);
+                break;
+            case "flip left":
+                help_page.transform.GetChild(1).GetChild(helpPageNum).gameObject.SetActive(false);
+                if (helpPageNum == 0)
+                {
+                    helpPageNum = 2;
+                } else
+                {
+                    helpPageNum -= 1;
+                }
+                help_page.transform.GetChild(1).GetChild(helpPageNum).gameObject.SetActive(true);
+                break;
+            case "flip right":
+                help_page.transform.GetChild(1).GetChild(helpPageNum).gameObject.SetActive(false);
+                if (helpPageNum == 2)
+                {
+                    helpPageNum = 0;
+                } else
+                {
+                    helpPageNum += 1;
+                }
+                help_page.transform.GetChild(1).GetChild(helpPageNum).gameObject.SetActive(true);
+                break;
         }
     }
 
